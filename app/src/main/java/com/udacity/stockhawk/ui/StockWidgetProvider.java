91ca259d377f73cd13a5f7bf26ee668555fccc98
Yin Zheng <yinzheng@gmail.com>
2016-11-28 14:52:25 +1100

    feature: add new widget

---
+++ app/src/main/java/com/udacity/stockhawk/ui/StockWidgetProvider.java
@@ -0,0 +1,33 @@
+package com.udacity.stockhawk.ui;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.udacity.stockhawk.R;
+
+/**
+ * Created by Iris on 25/11/2016.
+ */
+
+public class StockWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(
+                    context.getPackageName(),
+                    R.layout.widget_stock);
+
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent
+                    .getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
